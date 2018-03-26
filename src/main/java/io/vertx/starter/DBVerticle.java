d1ae07260fdae22f85e39c309481d556bc541c91
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2018-03-26 13:11:53 -0500

    initial project

---
+++ src/main/java/io/vertx/starter/DBVerticle.java
@@ -0,0 +1,58 @@
+package io.vertx.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.mongo.MongoClient;
+
+public class DBVerticle extends AbstractVerticle {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(DBVerticle.class);
+
+  MongoClient mongoClient;
+
+  private static final String DB_QUEUE = "db.queue";
+
+
+  public void start(Future<Void> startFuture) throws Exception{
+    // Configure the MongoClient inline.  This should be externalized into a config file
+    mongoClient = MongoClient.createShared(vertx, new JsonObject().put("db_name", "mydb").put("connection_string", "mongodb://127.0.0.1:37017"));
+    vertx.eventBus().consumer(DB_QUEUE, this::onMessage);
+    startFuture.complete();
+  }
+
+  void onMessage(Message<JsonObject> message){
+
+    LOGGER.debug("message received");
+
+    // if our message does not contain an action
+    if(!message.headers().contains("query")){
+      message.fail(1, "No Query Specified!");
+    }
+
+    switch (message.headers().get("query")){
+
+      case "get-all":
+        this.getAll(message);
+    }
+  }
+
+  private void getAll(Message<JsonObject> message) {
+
+        mongoClient.find("stuff", new JsonObject(), res -> {
+            if(res.succeeded()){
+                System.out.println(res.result());
+                message.reply(new JsonObject().put("results", Json.encodePrettily(res.result())));
+            }else{
+                System.out.println(res.cause());
+                message.reply(new JsonObject().put("result", "failed").put("cause", res.cause().getCause()));
+            }
+        });
+
+  }
+
+}
--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,14 +1,65 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private static final String DB_QUEUE = "db.queue";
+
   @Override
-  public void start() {
-    vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x!"))
-        .listen(8080);
+  public void start(Future<Void> startFuture) {
+
+    //Create router
+    Router router = Router.router(vertx);
+    router.route("/").handler(routingContext -> {
+      HttpServerResponse response = routingContext.response();
+      response.putHeader("content-type", "text/html")
+        .end("<h1>Hello, World!</h1>");
+    });
+
+    router.get("/api/stuff").handler(this::getAll);
+
+    // Create the HTTP server and pass the "accept" method to the request handler.
+    vertx
+      .createHttpServer()
+      .requestHandler(router::accept)
+      .listen(
+        // Retrieve the port from the configuration,
+        // default to 8080.
+        config().getInteger("http.port", 8080),
+        result -> {
+          if (result.succeeded()) {
+            startFuture.complete();
+          } else {
+            startFuture.fail(result.cause());
+          }
+        }
+      );
+  }
+
+  private void getAll(RoutingContext routingContext){
+    vertx.eventBus().send(DB_QUEUE, new JsonObject(), new DeliveryOptions().addHeader("query", "get-all"), reply ->{
+      if(reply.succeeded()){
+        JsonObject body = (JsonObject) reply.result().body();
+        routingContext.response()
+          .setStatusCode(200)
+          .putHeader("content-type", "application/json; charset=utf-8")
+          .end(Json.encodePrettily(body));
+      }else {
+        System.out.println(reply.cause());
+        routingContext.response()
+          .setStatusCode(500)
+          .putHeader("content-type", "application/json; charset=utf-8")
+          .end(Json.encodePrettily(reply.cause()));
+      }
+    });
   }
 
 }
